8f7c040080a2fe2eb3d13b21bc19ed07384891b6
Christoph <christoph@hindsihgt.com.au>
2024-09-13 09:56:18 +1000

    Refactor Kubota_Connect_Cron to schedule single cron jobs with specified schedule

--- includes/class-kubota-connect-cron-jobs.php
+++ includes/class-kubota-connect-cron-jobs.php
@@ -53,6 +53,7 @@ class Kubota_Connect_Cron {
      */
     private function schedule_single_cron_job($cpt, $hook) {
         $schedule = get_option($cpt . '_schedule', 'daily');
+        error_log("Scheduling cron job for $hook with schedule $schedule");
 
         $timestamp = wp_next_scheduled($hook);
         if ($timestamp) {
